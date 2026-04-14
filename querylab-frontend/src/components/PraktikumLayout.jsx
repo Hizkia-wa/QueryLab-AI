f72555f17c59d10d558d95361ba186b3ac80c4e1
Hizkia-wa <siahaanhizkia06@gmail.com>
2026-04-14 20:10:13 +0700

    Update PraktikumLayout.jsx

--- querylab-frontend/src/components/PraktikumLayout.jsx
+++ querylab-frontend/src/components/PraktikumLayout.jsx
@@ -1,7 +1,6 @@
 import { useState, useEffect } from "react";
 import { useParams, Link } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
-const baseSoal = {
 import soalData from "../data/soal";
 
 export default function PraktikumLayout() {
