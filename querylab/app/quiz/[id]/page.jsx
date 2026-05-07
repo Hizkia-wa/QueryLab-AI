60cd39ff08ee1a3ed838af4adf56ed75ccb8ada8
NovsiSilalahi08 <novsianasilalahi06@gmail.com>
2026-05-07 08:46:59 +0700

    Update page.jsx

--- querylab/app/quiz/[id]/page.jsx
+++ querylab/app/quiz/[id]/page.jsx
@@ -1,5 +1,11 @@
 import QuizPage from "../../pages/QuizPage";
 
-export default function QuizRoute({ params }) {
-  return <QuizPage id={params.id} />;
+export default async function QuizRoute(props) {
+
+  const params = await props.params;
+  const id = params.id;
+
+  console.log("QUIZ ID:", id);
+
+  return <QuizPage id={id} />;
 }
